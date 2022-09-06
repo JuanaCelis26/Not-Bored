c78a26359413b90d0a22a696cf3ae4ff3a6d5ecf
dinampuez <dara.inampuez@mercadolibre.com.co>
2022-09-06 18:20:28 -0500

    Terms and conditions

--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.notbored
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val termAndConditionsTv= findViewById<TextView>(R.id.tv_TermsAndConditions)
+        termAndConditionsTv.setOnClickListener {
+        val intent= Intent(this, TermAndConditions::class.java)
+        startActivity(intent)
+        }
+
     }
 }
