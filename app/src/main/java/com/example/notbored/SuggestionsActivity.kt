77b54ca4c0fca6c608d53353197f6374d91aebe0
dinampuez <dara.inampuez@mercadolibre.com.co>
2022-09-07 11:13:46 -0500

    Resolve conflicts

--- app/src/main/java/com/example/notbored/SuggestionsActivity.kt
+++ app/src/main/java/com/example/notbored/SuggestionsActivity.kt
@@ -3,7 +3,7 @@ package com.example.notbored
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class suggestionsActivity : AppCompatActivity() {
+class SuggestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suggestions)
